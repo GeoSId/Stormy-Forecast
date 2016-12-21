d8d5002437a2b2b942dbf9c09d35f27454ea5ae2
geosid <georgsider@yahoo.gr>
2016-12-21 14:05:26 +0200

    api

--- app/src/main/java/weatherTeam/com/forecast/api/ApiInterface.java
+++ app/src/main/java/weatherTeam/com/forecast/api/ApiInterface.java
@@ -6,7 +6,7 @@ import retrofit2.http.Query;
 import weatherTeam.com.forecast.model.ForecastResponse;
 
 public interface ApiInterface {
-    @GET("find?&cnt=2&&cnt=2&appid= {YOUR API KEY} &units=imperial")
+    @GET("find?&cnt=2&&cnt=2&appid= {Your Api Key }&units=imperial")
     Call<ForecastResponse> SearchCoords( @Query("lat") double lat,
                                               @Query("lon") double lon);
 }
